4e75ac6027fed2de29ffa61661ca136289486e2f
Michał <m.krepiniewicz@gmail.com>
2024-03-14 18:33:54 +0100

    Warsaw - not very fond of this task rev.01

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -28,17 +28,15 @@ class TestTextNode(unittest.TestCase):
         self.assertNotEqual(node, node2)
 
     def test_eq_url(self):
-        node = TextNode("This is a text node", text_type_italic, "https://www.boot.dev")
-        node2 = TextNode(
-            "This is a text node", text_type_italic, "https://www.boot.dev"
-        )
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
         self.assertEqual(node, node2)
 
-    # def test_repr(self):
-    #     node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
-    #     self.assertEqual(
-    #         "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
-    #     )
+    def test_repr(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
 
 
 if __name__ == "__main__":
